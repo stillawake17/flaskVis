af11a1dd1b3c4abf5bf2fbf358bb818b1f31ff7a
Ioanna Papageorgiou <ioanna2.papageorgiou@uwe.ac.uk>
2023-10-06 11:13:52 +0100

    "updated some things"

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ import logging
 DATA_PATH = "C:/Users/josti/OneDrive/Documents/01 - Joanna/Local council/0000 - Bristol Airport/Arrivals_August.xlsx"
 
 def to_rgb(color):
-    """Convert a hex color string to an RGB tuple."""
+    "Convert a hex color string to an RGB tuple."
     color = color.lstrip('#')
     return tuple(int(color[i:i+2], 16) / 255.0 for i in (0, 2, 4))
 
 def to_hex(rgb):
-    """Convert an RGB tuple to a hex color string."""
+    "Convert an RGB tuple to a hex color string."
     return "#{:02x}{:02x}{:02x}".format(int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255))
 
 def lighten_color(color, factor=0.5):
@@ -28,13 +28,11 @@ def lighten_color(color, factor=0.5):
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-
 def load_and_process_data(selected_date):
     logging.info(f"Selected date: {selected_date}")
 
     # Loading the dataset for arrivals
-    # df = pd.read_excel("/mnt/data/Arrivals_August.xlsx")
-    df = pd.read_excel("C:/Users/josti/OneDrive/Documents/01 - Joanna/Local council/0000 - Bristol Airport/Arrivals_August.xlsx")
+    df = pd.read_excel(DATA_PATH)
     
     # Convert LandedDate and LandedTime to a single datetime column
     df["FullLandedTime"] = pd.to_datetime(df["LandedDate"].astype(str) + ' ' + df["LandedTime"])
@@ -60,10 +58,8 @@ def load_and_process_data(selected_date):
 
     return df, start_date_arr, end_date_arr
 
-
 def generate_progress_bars_v3(actual_values, quotas, categories, active_color, muted_color):
-    """Generate a plot with progress bars for given actual values, quotas, and categories."""
-    # Placeholder implementation
+    "Generate a plot with progress bars for given actual values, quotas, and categories."
     img = io.BytesIO()
     plt.bar(categories, actual_values, color=active_color, label="Actual Values")
     plt.bar(categories, quotas, color=muted_color, label="Quotas", bottom=actual_values)
@@ -73,13 +69,11 @@ def generate_progress_bars_v3(actual_values, quotas, categories, active_color, m
     return img
 
 def generate_plot(df, start_date_arr, end_date_arr):
-    # ... (same as in your original code) ...
     logging.info("Generating plot...")
 
     # Load the entire dataset to calculate total percentages
-    df_total = pd.read_excel("C:/Users/josti/OneDrive/Documents/01 - Joanna/Local council/0000 - Bristol Airport/Arrivals_August.xlsx")
+    df_total = pd.read_excel(DATA_PATH)
     
-    #df_total = pd.read_excel("/mnt/data/Arrivals_August.xlsx")
     df_total["FullLandedTime"] = pd.to_datetime(df_total["LandedDate"].astype(str) + ' ' + df_total["LandedTime"])
     df_total["Hour"] = df_total["FullLandedTime"].dt.hour
     df_total["Minute"] = df_total["FullLandedTime"].dt.minute
@@ -107,34 +101,8 @@ def generate_plot(df, start_date_arr, end_date_arr):
         "Regular arrivals": "blue"
     }
 
-    
-# Generating progress bars
-#actual_values = [night_hour_percentage * allowed_quota_night / 100, shoulder_hour_percentage * allowed_quota_shoulder / 100, total_flight_percentage * allowed_quota_total / 100]
-#quotas = [allowed_quota_night, allowed_quota_shoulder, allowed_quota_total]
-#categories = ["Night hour flights", "Shoulder hour flights", "Total flights"]
-#img = generate_progress_bars_v3(actual_values, quotas, categories, 'red', '#E0E0E0')
-
-
-    Generate a plot with progress bars for given actual values, quotas, and categories.
-    Each progress bar will show the percentage of the actual value against its quota.
-    
-    Parameters:
-        actual_values (list): List of actual values for each category.
-        quotas (list): List of quotas for each category.
-        categories (list): List of category names.
-        active_color (str): Color for the current percentage.
-        muted_color (str): Color for the remaining percentage.
-        
-    Returns:
-        BytesIO: BytesIO object containing the PNG image of the plot.
-    
-def generate_progress_bars_v3<code object generate_progress_bars_v3 at 0x7ef4df4739d0, file "/tmp/ipykernel_51/725549664.py", line 1>
-
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-    plt.close()
-    
-    return plot_url
+    # The rest of the code for this function seems to be missing or was truncated in the provided input.
+    # It might need to be added back in later.
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -146,7 +114,44 @@ def index():
     image_filename = 'default_image.png'
 
     if request.method == 'POST':
-        # ... (same as in your original code) ...
+        selected_date = request.form['selected_date']
+        df, start_date_arr, end_date_arr = load_and_process_data(selected_date)
+        if df.empty:
+            message = f"No data found for the selected date: {selected_date}"
+        else:
+            plot_url = generate_plot(df, start_date_arr, end_date_arr)
+
+             # Load the entire dataset to calculate total percentages
+            df_total = pd.read_excel("C:/Users/josti/OneDrive/Documents/01 - Joanna/Local council/0000 - Bristol Airport/Arrivals_August.xlsx")
+            df_total["FullLandedTime"] = pd.to_datetime(df_total["LandedDate"].astype(str) + ' ' + df_total["LandedTime"])
+            df_total["Hour"] = df_total["FullLandedTime"].dt.hour
+            df_total["Minute"] = df_total["FullLandedTime"].dt.minute
+            df_total["Time_Category"] = "Regular arrivals"
+            df_total.loc[(df_total["Hour"] == 23) & (df_total["Minute"] < 30), "Time_Category"] = "Shoulder hour flights"
+            df_total.loc[(df_total["Hour"] == 23) & (df_total["Minute"] >= 30), "Time_Category"] = "Night hour arrivals"
+            df_total.loc[(df_total["Hour"] < 6), "Time_Category"] = "Night hour arrivals"
+            df_total.loc[(df_total["Hour"] == 6), "Time_Category"] = "Shoulder hour flights"
+
+            # Calculate total percentages
+            total_counts = df_total["Time_Category"].value_counts()
+            total_percentages = ((total_counts / total_counts.sum()) * 100).round(1)
+
+            # Calculate day percentages
+            day_counts = df["Time_Category"].value_counts()
+            day_percentages = ((day_counts / day_counts.sum()) * 100).round(1)
+
+            # Create summary table
+            summary_df = pd.DataFrame({'Number of Flights (Day)': day_counts, 'Percentage (Day) %': day_percentages,
+                                       'Number of Flights (Total)': total_counts, 'Percentage (Total) %': total_percentages})
+
+            # Add total row
+            total_day_flights = day_counts.sum()
+            total_all_flights = total_counts.sum()
+            summary_df.loc['Total'] = [total_day_flights, 100, total_all_flights, 100]  # 100% since it's the total
+
+            # Convert to HTML for rendering
+            summary = summary_df.to_html(classes='summary-table')
+
 
     return render_template('index.html', plot_url=plot_url, message=message, summary=summary, night_hour_percentage=night_hour_percentage, image_filename=image_filename)
 
